de5c4f588137693643b6c0671f5eed59f0fee934
ORIFJON NARKULOV <orifjon9@gmail.com>
2017-09-07 10:46:26 -0500

    Add project files.

---
+++ ps-start2aspnetmvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ps_start2aspnetmvc.MvcApplication" Language="C#" %>
---
+++ ps-start2aspnetmvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ps_start2aspnetmvc.Startup))]
+namespace ps_start2aspnetmvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
